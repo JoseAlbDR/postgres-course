81908781bd2d711489bdafa27b51abd14b1f0e2a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-29 11:29:07 +0100

    feat: aggregation function for stadistics

--- tareas/calentamiento.sql
+++ tareas/calentamiento.sql
@@ -24,3 +24,25 @@ WHERE
     followers > 4600
 ORDER BY
     followers DESC;
+    
+    
+SELECT
+    COUNT(*)
+FROM
+    users
+WHERE
+    followers > 4600;
+    
+
+SELECT
+    first_name,
+    last_name,
+    followers
+FROM
+    users
+WHERE
+--     followers > 4600
+--     AND followers < 4700
+	followers BETWEEN 4600 and 4700
+ORDER BY
+    followers ASC;
