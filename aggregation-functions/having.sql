143e8f9eacc8dd48e149ee2c74afaeef28f92d6b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-29 12:13:03 +0100

    feat: having with another functions

--- aggregation-functions/having.sql
+++ aggregation-functions/having.sql
@@ -10,5 +10,29 @@ HAVING
 ORDER BY
 	COUNT(*) DESC;
     
+SELECT * FROM users;
+
+SELECT
+    DISTINCT(country)
+FROM
+    users;
     
+SELECT
+    COUNT(email),
+    SUBSTRING(email, POSITION('@' in email) + 1) as domain
+FROM
+    users
+GROUP BY
+    SUBSTRING(email, POSITION('@' in email) + 1)
+HAVING
+    count(email) > 1
+ORDER BY
+    COUNT(email) DESC;
+    
+select
+    email
+from
+    users
+where
+    email like '%google.com';
 
