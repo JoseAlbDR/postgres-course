ee0e4aaae885a8d3e0852c368b5e77d2794e0be4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-30 16:56:39 +0100

    feat: more index examples

--- index/index.sql
+++ index/index.sql
@@ -5,3 +5,10 @@ create UNIQUE index "unique_country_name" on country(name);
 SELECT * from country where continent = 'Africa';
 
 create index "country_continent" on country(continent);
+
+select * from city where name='Jinzhou' and countrycode = 'CHN' and district = 'Liaoning';
+
+create unique index "unique_name_countrycode_district" on city(name, countrycode, district);
+
+create index "index_district" on city(district);
+
