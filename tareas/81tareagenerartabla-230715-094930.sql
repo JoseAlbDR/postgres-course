dd53adf7cce1c90f33e37578814b2ad210a610b9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-01-31 11:57:56 +0100

    feat: union task complete

--- tareas/81tareagenerartabla-230715-094930.sql
+++ tareas/81tareagenerartabla-230715-094930.sql
@@ -1,5 +1,3 @@
-
-
 -- Count Union - Tarea
 -- Total |  Continent
 -- 5	  | Antarctica
@@ -30,3 +28,23 @@ from
 where
     b.name like ('%America')
 order by Total asc;
+-- Quiero que me muestren el pais con mas ciudades
+-- Campos: total de ciudades y el nombre del pais
+-- usar INNER JOIN
+
+
+
+select
+    count(*) as total,
+    b.name as country
+from
+    city a
+    inner join country b on a.countrycode = b.code
+group by
+    b.name
+order by
+    count(*) desc
+limit
+    1;
+
+
